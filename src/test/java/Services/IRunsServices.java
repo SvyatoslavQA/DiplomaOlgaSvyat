1851624075464706790a3b5d56a129a986087105
Volha Milk <olkamiroyka@gmail.com>
2024-06-03 18:19:25 +0300

    add interfaces add services change methods in RQ on metgods

--- src/test/java/Services/IRunsServices.java
+++ src/test/java/Services/IRunsServices.java
@@ -1,8 +1,6 @@
 package Services;
 
 import models.Run;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public interface IRunsServices {
     Run completeRunUsingFile(int runID);
